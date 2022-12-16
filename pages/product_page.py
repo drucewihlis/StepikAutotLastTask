2e48a94cb2fabab15f0145ca7d94be8d89187d40
anvi <avikhlyantsev@fix.ru>
2022-12-16 16:18:23 +0300

    enrich test case: no success message when open page

--- pages/product_page.py
+++ pages/product_page.py
@@ -11,14 +11,18 @@ class ProductPage(BasePage):
 
     def should_be_bookname_in_notify(self, browser):
         book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
-        real_name = self.browser.find_element(*ProductPageLocators.NOTIFY_ADDED_TO_BASKET).text
         WebDriverWait(browser, 5).until(
             EC.text_to_be_present_in_element((ProductPageLocators.NOTIFY_ADDED_TO_BASKET), book_name))
+        real_name = self.browser.find_element(*ProductPageLocators.NOTIFY_ADDED_TO_BASKET).text
         assert book_name == real_name, f"FOUND {real_name}\nEXPECTED {book_name}"
 
     def should_be_price_in_notify(self, browser):
         book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
-        real_price = self.browser.find_element(*ProductPageLocators.NOTIFY_BASKET_TOTAL).text
         WebDriverWait(browser, 5).until(
             EC.text_to_be_present_in_element((ProductPageLocators.NOTIFY_BASKET_TOTAL), book_price))
+        real_price = self.browser.find_element(*ProductPageLocators.NOTIFY_BASKET_TOTAL).text
         assert book_price == real_price, f"FOUND {real_price}\nEXPECTED {book_price}"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.NOTIFY_ADDED_TO_BASKET), \
+            "Success message is presented, but should not be"
