48cf6e64a42a3c36e8e36b9c3b9d619121a7bb30
anvi <avikhlyantsev@fix.ru>
2022-12-21 16:00:48 +0300

    move methods mainpage->basepage + 2 new tests on productpage

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,14 +3,26 @@ import math
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
+from .locators import BasePageLocators
+
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
 
-    def open(self):
-        self.browser.get(self.url)
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
 
     def is_element_present(self, how, what):
         try:
@@ -27,14 +39,11 @@ class BasePage():
 
         return False
 
-    def is_disappeared(self, how, what, timeout=4):
-        try:
-            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
-                until_not(EC.presence_of_element_located((how, what)))
-        except TimeoutException:
-            return False
+    def open(self):
+        self.browser.get(self.url)
 
-        return True
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 import pytest
 import time
 
+from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 from .pages.locators import ProductPageLocators
 
@@ -44,3 +45,17 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.click_add_to_basket()
     assert page.is_disappeared(*ProductPageLocators.NOTIFY_ADDED_TO_BASKET)
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
