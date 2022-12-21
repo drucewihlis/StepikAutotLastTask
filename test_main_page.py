ba71910da70d09f070f078009dced533fe8dfe58
anvi <avikhlyantsev@fix.ru>
2022-12-21 18:53:43 +0300

    add test to check empty basket from product page

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 import pytest
 import time
 
+from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 from .pages.locators import ProductPageLocators
@@ -59,3 +60,13 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()  # Гость открывает страницу товара
+    page.go_to_basket_page() # Переходит в корзину по кнопке в шапке
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_no_items_in_basket() # Ожидаем, что в корзине нет товаров
+    basket_page.should_be_text_empty_basket(browser) # Ожидаем, что есть текст о том что корзина пуста
+
